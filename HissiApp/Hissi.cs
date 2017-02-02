62cbbed7e08bf4823db02ba78ed75705090bab20
Jussi <K8270@student.jamk.fi>
2017-02-02 14:52:22 +0200

    Hissi valmis

---
+++ HissiApp/Hissi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HissiApp
+{
+    class Hissi
+    {
+        private readonly int maxKerros = 5;
+        private readonly int minKerros = 1;
+
+        private int kerros;
+        public int Kerros
+        {
+            get
+            {
+                return kerros;
+            }
+            set
+            {
+                if (value <= maxKerros && value >= minKerros) // jos kerros on välillä 1-5
+                {
+                    kerros = value;
+                }
+                else
+                {
+                    Console.WriteLine("Hissi ei kulje tuohon kerrokseen. Valitse kerros 1-5.");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ HissiApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HissiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hissi hissi = new Hissi();
+
+            do
+            {
+                Console.Write("Anna kerros jonne haluat mennä. Valitse 1-5 >");
+                string syote = Console.ReadLine();
+                hissi.Kerros = int.Parse(syote);
+
+                Console.WriteLine("Hissi kerroksessa: " + hissi.Kerros);
+
+            } while (1 == 1);
+            
+        }
+    }
+}
