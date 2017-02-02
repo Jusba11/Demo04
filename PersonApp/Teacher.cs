1f03b11614a0abe9b4cb257030c38f0e64ada72c
Jussi <K8270@student.jamk.fi>
2017-02-02 14:13:34 +0200

    valmis

---
+++ PersonApp/Teacher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    class Teacher : Person
+    {
+        public string Room { get; set; }
+
+        // default constructor
+        public Teacher()
+        {
+        }
+
+        // parametric constuctor calls base class constructor with parameters
+        public Teacher(string firstName, string lastName, string room)
+            : base(firstName, lastName)
+        {
+            Room = room;
+        }
+
+        // just one method what Teacher can do 
+        public void TeacherMethod()
+        {
+            Console.WriteLine("This method belongs to Teacher!");
+        }
+
+        // override base class ToString()-method
+        public override string ToString()
+        {
+            //base on tässä tapauksessa Person-luokka
+            //kutsutaan Person luokan ToString()-metodia.
+            return base.ToString() + " " + Room;
+        }
+    }
+}
